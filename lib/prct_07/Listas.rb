fa06656e7e7227cd403a9edcfa632ce5f68f0843
alu0100782851 <alu0100782851@ull.edu.es>
2015-11-11 13:48:13 +0000

    Definiendo metodo para insertar por la cabeza

--- lib/prct_07/Listas.rb
+++ lib/prct_07/Listas.rb
@@ -9,4 +9,18 @@ class Lista
         @tail = nil #fin de la lista
         @contador=0  #variable que determina el tamaño actual de la lista
     end
+    
+    def insert_head(nodo)
+        
+        if empty == true
+            nodo.siguiente = nil
+            @head = nodo
+            @tail = nodo
+        else
+            nodo.siguiente = @head
+            @head = nodo
+        end
+        @contador = @contador + 1
+        
+    end
 end
