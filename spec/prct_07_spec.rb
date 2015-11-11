f71dcec4fc315cacfb82cc648109623436fccfe6
alu0100782851 <alu0100782851@ull.edu.es>
2015-11-11 13:37:55 +0000

    Comenzando fichero de pruebas

---
+++ spec/prct_07_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'lib/prct_07/Bibliografias'
+require 'lib/prct_07/Listas'
+
+describe Bibliografias do
+
+  before :each do
+    #referencias almacenadas como objetos de la clase bibliografias
+    @libro_1 = Bibliografias.new("Dave Thomas, Andy Hunt, Chad Fowler","Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide. (The Facets of Ruby)","Pragmatic Bookshelf","4","July 7, 2013","13-1937785499 , 10-1937785491")
+    @libro_2 = Bibliografias.new("Scott Chacon","Pro Git 2009th Edition. (Pro)","Apress","2009","August 27, 2009", "978-1430218333 , 1430218339")
+    @libro_3 = Bibliografias.new("David Flanagan, Yukihiro Matsumoto","The Ruby Programming Language","O’Reilly Media","1","February 4, 2008","0596516177, 978-0596516178")
+    @libro_4 = Bibliografias.new("David Chelimsky, Dave Astels, Bryan Helmkamp, Dan North, Zach Dennis, Aslak Hellesoy","The RSpec Book: Behaviour Driven Development with RSpec, Cucumber, and Friends (The Facets of Ruby)","Pragmatic Bookshelf","1","December 25, 2010","1934356379, 978-1934356371")
+    @libro_5 = Bibliografias.new("Richard E. Silverman","Git Pocket Guide","O’Reilly Media","1","August 2, 2013","1449325866, 978-1449325862")
+  end
+  
+end
